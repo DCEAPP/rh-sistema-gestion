726a3e3ba214aa31281e58f8e564bcfe015007be
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2022-12-07 16:08:11 -0600

    cambios

--- src/app/gestion-rh/gestion-rh.module.ts
+++ src/app/gestion-rh/gestion-rh.module.ts
@@ -6,6 +6,11 @@ import {SharedModule} from '../shared/shared.module';
 import {NzButtonModule} from 'ng-zorro-antd/button';
 import {PruebaComponent} from './prueba/prueba.component';
 import {GestionRhRoutingModule} from './gestion-rh-routing.module';
+import {NzSwitchModule} from "ng-zorro-antd/switch";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzDividerModule} from "ng-zorro-antd/divider";
+import {NzGridModule} from "ng-zorro-antd/grid";
 
 /** Assign all ng-zorro modules to this array*/
 const antdModule = [
@@ -16,7 +21,12 @@ const antdModule = [
   imports: [
     SharedModule,
     GestionRhRoutingModule,
-    ...antdModule
+    ...antdModule,
+    NzSwitchModule,
+    NzCardModule,
+    NzTableModule,
+    NzDividerModule,
+    NzGridModule
   ],
   exports: [],
   declarations: [
--- src/app/shared/template/side-nav/side-nav-routes.config.ts
+++ src/app/shared/template/side-nav/side-nav-routes.config.ts
@@ -17,23 +17,164 @@ export const ROUTES: SideNavInterface[] = [
     icon: 'appstore',
     submenu: [
       {
-        path: '/gestion-rh/prueba',
-        title: 'Nivel 1',
-        iconType: '',
-        icon: '',
-        iconTheme: '',
-        submenu: []
-      },
-      {
-        path: '',
-        title: 'Nivel 2',
+        path: '/gestion-rh/invitado',
+        title: 'Invitado',
         iconType: '',
         icon: '',
         iconTheme: '',
         submenu: [
           {
-            path: '',
-            title: 'Nivel 3',
+            path: '/gestion-rh/invitado/vacantes',
+            title: 'Vacantes',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/invitado/solicitudes',
+            title: 'Hacer Solicitud',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/invitado/evaluacion/prueba-conocimiento',
+            title: 'Prueba de Conocimiento',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/invitado/evaluacion/prueba-inteligencia',
+            title: 'Prueba de Inteligencia',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/invitado/evaluacion/prueba-psicometrico',
+            title: 'Prueba Psicometrica',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          }
+        ]
+      },
+      {
+        path: '/gestion-rh/puestos',
+        title: 'Puestos',
+        iconType: '',
+        icon: '',
+        iconTheme: '',
+        submenu: [
+          {
+            path: '/gestion-rh/puestos/organigrama',
+            title: 'Organigrama',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/puestos/vacantes',
+            title: 'Vacantes',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/puestos/contrataciones',
+            title: 'Contrataciones',
+            iconType: '',
+            icon: '',
+            iconTheme: '',
+            submenu: []
+          },
+        ]
+      },
+      {
+        path: '/gestion-rh/proyectos',
+        title: 'Proyectos',
+        iconType: '',
+        icon: '',
+        iconTheme: '',
+        submenu: [
+          {
+            path: '/gestion-rh/proyectos/registro-proyectos',
+            title: 'Registro',
+            iconType: 'nzIcon',
+            iconTheme: 'outline',
+            icon: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/proyectos/consulta-proyectos',
+            title: 'Consulta',
+            iconType: 'nzIcon',
+            iconTheme: 'outline',
+            icon: '',
+            submenu: []
+          }
+        ]
+      },
+      {
+        path: '/gestion-rh/solicitudes',
+        title: 'Solicitudes',
+        iconType: '',
+        icon: '',
+        iconTheme: '',
+        submenu: [
+          {
+            path: '/gestion-rh/solicitudes/consulta-solicitudes',
+            title: 'Consulta',
+            iconType: 'nzIcon',
+            iconTheme: 'outline',
+            icon: '',
+            submenu: []
+          },
+        ]
+      },
+      {
+        path: '/gestion-rh/evaluaciones',
+        title: 'Evaluaciones',
+        iconType: '',
+        icon: '',
+        iconTheme: '',
+        submenu: [
+          {
+            path: '/gestion-rh/solicitudes/consulta-evaluacion',
+            title: 'Consulta',
+            iconType: 'nzIcon',
+            iconTheme: 'outline',
+            icon: '',
+            submenu: []
+          }
+        ]
+      },
+      {
+        path: '/gestion-rh/mantenimiento',
+        title: 'Mantenimiento',
+        iconType: '',
+        icon: '',
+        iconTheme: '',
+        submenu: [
+          {
+            path: '/gestion-rh/mantenimiento/catalogo-carreras',
+            title: 'Catalogo Carreras',
+            iconType: 'nzIcon',
+            iconTheme: 'outline',
+            icon: '',
+            submenu: []
+          },
+          {
+            path: '/gestion-rh/solicitudes/catalogo-habilidades',
+            title: 'Catalogo Habilidades',
             iconType: 'nzIcon',
             iconTheme: 'outline',
             icon: '',
