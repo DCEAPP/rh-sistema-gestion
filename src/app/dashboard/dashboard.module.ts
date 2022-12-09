87c391085a57b8cff76e16c0ad90af25b7f69395
Kevin David Molina Gómez <kevin.molina.go@gmail.com>
2022-12-08 21:00:11 -0600

    GRAFICAS TERMINADAS

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,6 +5,19 @@ import { DashboardComponent } from './dashboard.component';
 
 /** Import any ng-zorro components as the module required except icon module */
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzAvatarModule} from "ng-zorro-antd/avatar";
+import {NzRadioModule} from "ng-zorro-antd/radio";
+import {NgChartjsModule} from "ng-chartjs";
+import {NzBadgeModule} from "ng-zorro-antd/badge";
+import {NzTagModule} from "ng-zorro-antd/tag";
+import {NzTableModule} from "ng-zorro-antd/table";
+import {NzProgressModule} from "ng-zorro-antd/progress";
+import {NzDropDownModule} from "ng-zorro-antd/dropdown";
+import {NzTimelineModule} from "ng-zorro-antd/timeline";
+import {NzTabsModule} from "ng-zorro-antd/tabs";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
+import {NzCodeBoxModule} from "../shared/components/codebox/codebox.module";
 
 /** Assign all ng-zorro modules to this array*/
 const antdModule = [
@@ -12,11 +25,24 @@ const antdModule = [
 ]
 
 @NgModule({
-    imports: [
-        SharedModule,
-        DashboardRoutingModule,
-        ...antdModule
-    ],
+  imports: [
+    SharedModule,
+    DashboardRoutingModule,
+    ...antdModule,
+    NzCardModule,
+    NzAvatarModule,
+    NzRadioModule,
+    NgChartjsModule,
+    NzBadgeModule,
+    NzTagModule,
+    NzTableModule,
+    NzProgressModule,
+    NzDropDownModule,
+    NzTimelineModule,
+    NzTabsModule,
+    NzCheckboxModule,
+    NzCodeBoxModule
+  ],
     exports: [],
     declarations: [
         DashboardComponent
